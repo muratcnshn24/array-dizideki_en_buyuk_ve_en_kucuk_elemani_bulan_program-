7d5bb2161c318b551b70b838f1a8885a358ed18d
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 18:03:19 +0300

    Proje dosyası ekle.

---
+++ array(dizideki_en_buyuk_ve_en_kucuk_elemani_bulan_program)/Dizi.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace array_dizideki_en_buyuk_ve_en_kucuk_elemani_bulan_program_
+{
+    public class Dizi
+    {
+        ///<sammary>
+        /// tek boyutlu dizi oluşturmak için kullanılan fonksiyondur.
+        ///</sammary>
+        ///<param name="Limit"> dizinin uzunluğunu belirtir </param>
+        ///<returns> belirtilen uzunlukta rastgele sayıyılardan oluşmuş diziyi döner  </returns>
+        public int[] Olustur(int Limit)
+        {
+            int[] sayilar = new int[Limit];
+            Console.WriteLine("");
+            for (int i = 0; i < Limit; i++)
+            {
+                sayilar[i] = new Random().Next(1, 100);
+                Console.WriteLine("{0,3}", sayilar[i]);
+            }
+            Console.WriteLine(""); // dizi karakterleri yan yana yazılsın diye alt ve üste bu kısmı yazıp diziyi  araya aldım
+            return sayilar;
+
+        }
+
+        ///<sammary>
+        ///  Parametre olarak aldığı dizideki en büyük elemanı döner.
+        ///</sammary>
+        ///<param name="Dizi"> Dizi </param>
+        ///<returns></returns>
+        public static int EnBuyuk(int[] dizi)
+        {
+            int eb = dizi[0];
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (dizi[i] > eb) eb = dizi[i];
+
+            }
+            return eb;
+        }
+
+        ///<sammary>
+        ///  Parametre olarak aldığı dizideki en küçük elemanı döner.
+        ///</sammary>
+        ///<param name="Dizi"> Dizi </param>
+        ///<returns></returns>
+        public static int EnKucuk(int[] dizi)                  //statik yaparsan nesne üretmene gerek kalmaz
+        {
+            int ek = dizi[0];
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                if (dizi[i] < ek) ek = dizi[i];
+
+            }
+            return ek;
+        }
+    }
+}
---
+++ array(dizideki_en_buyuk_ve_en_kucuk_elemani_bulan_program)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace array_dizideki_en_buyuk_ve_en_kucuk_elemani_bulan_program_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("n degerini giriniz:");
+            int Limit = Convert.ToInt32(Console.ReadLine());
+
+           
+
+            int[] sayilar = new Dizi().Olustur(Limit);
+
+            Console.WriteLine("\n Dizinin En Büyük Elemani:{0}", Dizi.EnBuyuk(sayilar));
+            Console.WriteLine("\n Dizinin En Küçük Elemani:{0}", Dizi.EnKucuk(sayilar));
+        }
+
+     
+
+       
+    }
+}
